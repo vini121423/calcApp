7faeb9906d70f473f6c09ba8f5f8bef74de6db0b
vini121423 <vinicius.santos838@etec.sp.gov.br>
2020-06-05 15:42:46 -0300

    Pequenas mudanças no layout

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,19 +14,43 @@ export class Tab1Page {
 
   public somar(){
     this.resultado = (this.n1) + (this.n2);
+
+    if((this.n1 === null) || (this.n2 === null) || ( this.resultado === null)){
+      this.resultado = null;
+    }
   }
 
   public subtrair(){
     this.resultado = this.n1 - this.n2;
+
+    if((this.n1 === null) || (this.n2 === null) || ( this.resultado === null)){
+      this.resultado = null;
+    }
   }
 
   public multiplicar(){
     this.resultado = this.n1 * this.n2;
+
+    if((this.n1 === null) || (this.n2 === null) || ( this.resultado === null)){
+      this.resultado = null;
+    }
   }
 
   public dividir(){
     this.resultado = this.n1 / this.n2;
+
+    if((this.n1 === null) || (this.n2 === null) || ( this.resultado === null)){
+      this.resultado = null;
+    }
   }
+
+  public limpar(){
+    this.resultado = null;
+    this.n1 = null;
+    this.n2 = null;
+  }
+
+  
 
 
 
